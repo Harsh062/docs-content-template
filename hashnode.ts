415cc63e50c687fc149d508dc69fcdad0fa775a6
Harsh <harsh@hashnode.com>
2024-10-28 20:18:26 +0530

    Update prod project id

--- hashnode.ts
+++ hashnode.ts
@@ -1,6 +1,6 @@
 // This is ignored for the publish from GH - it is needed for the CLI and local previews
 // THe project ID is connected to the installation ID of the repo, thus we now which project to update
 export default {
-  linkedProjectId: "66826066f496f6d9063fec22",
-  previewProjectId: "66826066f496f6d9063fec22",
+  linkedProjectId: "665ea77792441d528c5638e9",
+  previewProjectId: "665ea77792441d528c5638e9",
 };
